402133195ca28f795761d4dafc2658821112aee0
Alexander Rudenko <alexander.e.rudenko@gmail.com>
2017-05-18 15:03:35 +0000

    Move webhook controller from closure to TransactionController Maed App\Transaction fillable

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -7,79 +7,21 @@ use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function webhook(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Transaction $transaction)
-    {
-        //
+        $endpoint = $request->url();
+        $inputs = json_decode(file_get_contents('php://input'), true);
+        if (isset($inputs['type']) && $inputs['type'] == 'verify') {
+            return hash('sha512', $endpoint);
+        }
+        if (isset($inputs['type']) && $inputs['type'] == 'btc_deposit') {
+            $inputs['amount_btc'] = $inputs['amount'];
+            unset($inputs['amount']);
+            unset($inputs['tx_expired']);
+            unset($inputs['type']);
+            $transaction = Transaction::create($inputs);
+            return $transaction->order;
+        }
+        return 1;
     }
 }
